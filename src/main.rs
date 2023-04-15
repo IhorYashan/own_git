54f3deb575fce3340e7b704981f4a58692423d2d
Ihor Yashan <ihor.yashan@intellias.com>
2023-04-15 20:25:16 +0300

    tree001

--- src/main.rs
+++ src/main.rs
@@ -89,7 +89,7 @@ fn parse_args(args: &String) -> (&str, &str) {
 fn read_tree_sha(sha_tree: String) {
     //let mut file_content = Vec::new();
 
-    let mut decoder = ZlibDecoder::new(sha_tree);
+    let mut decoder = ZlibDecoder::new(sha_tree.as_bytes());
 
     let mut buffer = [0; 4096];
 
@@ -128,6 +128,6 @@ fn main() {
     if args[1] == "ls-tree" && args[2] == "--name-only" {
         let sha_tree = &args[3];
 
-        read_tree_sha(sha_tree);
+        read_tree_sha(sha_tree.to_string());
     }
 }
