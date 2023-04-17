163f9a4029f4d425d4aec9d2c2f50daf74f8f55d
Ihor Yashan <ihor.yashan@intellias.com>
2023-04-17 22:59:47 +0300

    refact _ decode_func()

--- src/main.rs
+++ src/main.rs
@@ -123,18 +123,18 @@ fn read_tree_sha(sha_tree: String) {
        }
     */
 
-    println!("buffer {:?}", buffer);
+    //println!("buffer {:?}", buffer);
     formatted_buff = String::from_utf8_lossy(&buffer[8..bytes]).to_string();
-    println!("formatted_buff {}", formatted_buff);
+    //println!("formatted_buff {}", formatted_buff);
     let formatted_buff = formatted_buff.replace("\\x00", "\x00");
-    println!("formatted_buff {}", formatted_buff);
+    //println!("formatted_buff {}", formatted_buff);
     let formatted_buff = formatted_buff.replace("\\\\", "\\");
-    println!("formatted_buff {}", formatted_buff);
+    //println!("formatted_buff {}", formatted_buff);
     let parts: Vec<&str> = formatted_buff.split('\x00').collect();
 
-    println!("parts : {:?}", parts);
-    println!(" ----  ");
-    println!(" ---- ");
+    //println!("parts : {:?}", parts);
+    //println!(" ----  ");
+    //println!(" ---- ");
     for part in parts {
         if part.contains(' ') {
             if let Some(word) = part.split(' ').nth(1) {
