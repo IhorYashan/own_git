76408960826c6047f82238994a07891c0f43bbc0
Ihor Yashan <ihor.yashan@intellias.com>
2023-04-18 09:47:32 +0300

    refact_zlib

--- src/main.rs
+++ src/main.rs
@@ -93,11 +93,11 @@ fn read_tree_sha(sha_tree: String) {
     let mut full_path = File::open(&full_path).unwrap();
     full_path.read_to_end(&mut file_content).unwrap();
 
-    let mut formatted_buff = String::new();
+    //let mut formatted_buff = String::new();
     let compressed_data = &file_content[..];
     let (buffer, bytes) = decode_data(compressed_data);
 
-    formatted_buff = buffer;
+    let mut formatted_buff = String::from_utf8_lossy(buffer.as_bytes()).to_string();
 
     let formatted_buff = formatted_buff.replace("\\x00", "\x00");
     let formatted_buff = formatted_buff.replace("\\\\", "\\");
