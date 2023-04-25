c3599fd2e62ffbdb137da818714e7170a18f61ae
Ihor Yashan <ihor.yashan@intellias.com>
2023-04-25 21:22:42 +0300

    refact

--- src/lib.rs
+++ src/lib.rs
@@ -175,9 +175,7 @@ pub mod git {
 
     pub fn do_commit(tree_sha: String, commit_sha: String, message: String) -> String {
         let content_commit = format!(
-            "tree {}\nparent {}\n
-author ScotChacon <schacon@gmail.com> 1243040974 -0700\n
-committer ScotChacon <schacon@gmail.com> 1243040974 -0700\n\n",
+            "tree {}\nparent {}\nauthor ScotChacon <schacon@gmail.com> 1243040974 -0700\n`committer ScotChacon <schacon@gmail.com> 1243040974 -0700\n\n",
             tree_sha, commit_sha
         );
 
