7047b2090a0ae41328785600f8118e6218f42b26
Ihor Yashan <ihor.yashan@intellias.com>
2023-05-17 13:19:00 +0300

    test

--- src/lib.rs
+++ src/lib.rs
@@ -386,8 +386,8 @@ pub mod git {
             } else {
                 let blob_sha = entry.2;
 
-                let curr_dir = dir_name.clone().to_owned()
-                    + &format!("/{}/{}", &blob_sha[..2], &blob_sha[2..]);
+                let curr_dir =
+                    dir_name.clone().to_owned() + &format!("{}/{}", &blob_sha[..2], &blob_sha[2..]);
 
                 println!("curr_dir : {}", curr_dir);
                 let git_data = fs::read(curr_dir).unwrap();
